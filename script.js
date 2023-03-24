21636893d4d50e2ed86111146d2261ed760e86b0
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-03-24 22:10:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 function calculateMinCost() {
-  //your code 
-	var inputData=document.querySelector("#rope-lengths").value;
-	var arr=inputData.split(",");
-	
-	for(var i=0;i<arr.length;i++){
-		arr[i]=Number(arr[i]);
-	}
-	// alert(typeof arr[1])
-	var cost=0;
-	arr.sort(function(a,b) { return a-b; })
-	while(arr.length>1){
-		var newRope=arr[0]+arr[1];
-		cost+=newRope;
-		arr.splice(0,2);
-		arr.push(newRope);
-		arr.sort(function (a,b) { return a-b; })
-		// alert(arr);
-	}
-    document.querySelector("#result").textContent=cost;
-	
+  //your code here
+
+	 let str = document.querySelector("#rope-lengths").value;
+    let arr = str.split(", ");
+
+    for(let i in arr){
+        arr[i] =  Number(arr[i]);
+    }
+     let mincost=0;
+    while(arr.length!=1){
+        arr.sort( function (a,b){
+            return a-b;
+        });
+        let cost= arr[0] + arr[1];
+        mincost+=cost;
+        arr.splice(0,2);
+        arr.push(cost);
+    }
+
+
+    document.querySelector("#result").innerHTML=mincost;
   
-  
-  
-}  
+}
